131f2dc1a11135b640475fbd7affe101b39a9b3d
fuckyouvoldemort <logan.hayman@fairpoint.net>
2017-12-29 23:34:10 -0800

    Adding code

---
+++ README.txt
@@ -0,0 +1,4 @@
+el scrapo.py runs in a python console, and el scrapo.pyw runs in the background. 
+You will have to terminate the process in Task Manager to turn it off.
+
+The default interval between scrapes is 10 minutes.
---
+++ el scrapo.py
@@ -0,0 +1,35 @@
+#El Scrapo
+import urllib.request
+import datetime
+import time
+from bs4 import BeautifulSoup
+
+currentDT = datetime.datetime.now()
+
+print("Starting...")
+
+# Scrape Steampowered for the statistics of PropHunt
+def Scrape():
+    quote_page = "http://steamcommunity.com/sharedfiles/filedetails/?id=135509255"
+    page = urllib.request.urlopen(quote_page)
+
+    soup = BeautifulSoup(page, 'html.parser')
+
+    steam_stats_box = soup.find('table', attrs={'class': 'stats_table'})
+    steam_stats = steam_stats_box.text.strip().replace("Unique Visitors", " UV | ").replace("Current Subscribers", " CS | ").replace("Current Favorites", " CF").replace("\n", "")
+
+    # This is to make the tuple work with writing to files
+    data = (datetime.datetime.now().strftime("%m/%d/%y, %I:%M %p") + " | " + steam_stats + "\n")
+
+    # Write to text file
+    with open("Steam Workshop PropHunt Data.txt","a") as file:
+        file.write(data)
+        #file.close()
+        return(data)
+   
+#Scrape()
+    
+# Scrape every 10 minutes
+while 1:
+    Scrape()
+    time.sleep(600)
